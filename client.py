05de02eaac4c3e99ad52f649cbc6e4e70b395f06
Nagesh <136742447+Nagesh-go@users.noreply.github.com>
2025-02-17 16:16:12 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,15 @@
+from NetworkInterfaceLayer import NetworkInterfaceLayer
+from ApplicationLayer import ApplicationLayer
+from TransportLayer import TransportLayer
+
+def client():
+    network_interface = NetworkInterfaceLayer('127.0.0.1', 12345)
+    network_interface.connect()
+    app_layer = ApplicationLayer(TransportLayer())
+
+    app_layer.send_message("Hello, Server!", network_interface, network_interface.sock)
+    
+    network_interface.close()
+
+if __name__ == "__main__":
+    client()
